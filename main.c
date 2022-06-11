890876f2fbe9965f730f3dac76452a0395ba6cff
LucasAlmeida127 <lucasvinicio@ucl.br>
2022-06-11 16:27:50 -0300

    imprimindo  a saída

--- main.c
+++ main.c
@@ -12,5 +12,10 @@ int main(void) {
     int maior = vetor[0], menor = vetor[0];
     Funcao(pvetor, quantidade, &maior, &menor); // Função que retorna o valor máximo e mínimo de um vetor de inteiros
 
+    printf("\n%i",vetor[0]);
+    for (int i = 0; i < quantidade; i++){
+        printf(",%i",vetor[i]);
+    }
+    printf("\nMaximo: %i | Minimo: %i", maior, menor);
     return 0;
 }
