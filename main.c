23dbdab1d2b722648f820a79e3577b85255f01bc
LucasAlmeida127 <lucasvinicio@ucl.br>
2022-06-11 16:22:02 -0300

    criando as entradas

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include "MinhaBiblioteca.h"
 
-int main(void){
+int main(void) {
     int quantidade, vetor[30];
-    int maior = vetor[0], menor = vetor[0];
     int *pvetor = &vetor;
+
+    scanf("%i", &quantidade);
+    for (int i = 0; i < quantidade; i++) {
+        scanf("%i", &vetor[i]);
+    }
+    
+    int maior = vetor[0], menor = vetor[0];
+
     return 0;
 }
